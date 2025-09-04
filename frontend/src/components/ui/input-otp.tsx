6a99c5ed53e8e645a825383ca9e218862953cfeb
leatile mosimanyana <100219407+Cse21-034@users.noreply.github.com>
2025-09-04 18:16:11 +0200

    Update input-otp.tsx

--- frontend/src/components/ui/input-otp.tsx
+++ frontend/src/components/ui/input-otp.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
   React.ComponentPropsWithoutRef<typeof OTPInput>
->(({ className, containerClassName, ...props }, ref) => (
+>(({ className, containerClassName, children, ...props }, ref) => (
   <OTPInput
     ref={ref}
     containerClassName={cn(
@@ -17,7 +17,9 @@ const InputOTP = React.forwardRef<
     className={cn("disabled:cursor-not-allowed", className)}
     maxLength={6} // Add required maxLength prop
     {...props}
-  />
+  >
+    {children}
+  </OTPInput>
 ))
 InputOTP.displayName = "InputOTP"
 
